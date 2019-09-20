fbbd671c5edd770b1293ee349e3a62898f24fdf8
deepblarn <deepblarn@gmail.com>
2019-09-20 18:40:26 +0200

    Unify into single script

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
-	// "strconv"
-	"github.com/gocolly/colly"
-	"encoding/json"
-	"os"
 	"math/rand"
+	"strconv"
+
+	"github.com/gocolly/colly"
 )
+
+var crawlUrl = "https://www.amazon.es/s?k=nintendo+switch&ref=nb_sb_noss_1"
+var categoriesUrls = []string{}
+var productLinks = []string{}
+
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func main() {
+	fetchCategories()
+	visitAllPagesFromCategories()
+	fetchItemInfo()
+}
 
 func RandomString() string {
 	b := make([]byte, rand.Intn(10)+10)
@@ -18,30 +28,77 @@ func RandomString() string {
 	return string(b)
 }
 
-
-func main() {
+func fetchCategories() {
 	c := colly.NewCollector()
-	categoriesUrls := []string{}
-	crawlUrl := "https://www.amazon.es/s?k=nintendo+switch&ref=nb_sb_noss_1"
 
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", RandomString())
-		fmt.Println("Visiting", r.URL)
 	})
 
 	c.OnHTML("#departments", func(e *colly.HTMLElement) {
 		e.ForEach("a.a-link-normal.s-navigation-item", func(_ int, e *colly.HTMLElement) {
-			// fmt.Printf("CATEGORY: %s URL %s \n", e.ChildText("span.a-size-base.a-color-base"), e.Request.AbsoluteURL(e.Attr("href")))
-			categoriesUrls = append(categoriesUrls,  e.Request.AbsoluteURL(e.Attr("href")))
-			categoriesUrlsJSON, _ := json.Marshal(categoriesUrls)
-			jsonFile,_ := os.Create("./categories.json")
-			defer jsonFile.Close()
-			jsonFile.Write(categoriesUrlsJSON)
-			jsonFile.Close()
+			categoriesUrls = append(categoriesUrls, e.Request.AbsoluteURL(e.Attr("href")))
 		})
 	})
-	
-	c.Visit(crawlUrl)
 
-	fmt.Println(categoriesUrls)
+	c.Visit(crawlUrl)
+}
+
+func visitAllPagesFromCategories() {
+	c := colly.NewCollector()
+	activePage := 0
+	c.Limit(&colly.LimitRule{Parallelism: 10})
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", RandomString())
+	})
+
+	c.OnHTML(".a-selected a", func(e *colly.HTMLElement) {
+		fmt.Println("PAGE NUMBER-------------- " + e.Text)
+		currentPage, _ := strconv.Atoi(e.Text)
+		activePage = currentPage
+	})
+
+	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
+		e.ForEach("div.a-section.a-spacing-medium", func(_ int, e *colly.HTMLElement) {
+			var productLink string
+			e.ForEach("a.a-link-normal.a-text-normal", func(_ int, e *colly.HTMLElement) {
+				productLink = e.Request.AbsoluteURL(e.Attr("href"))
+				if productLink == "" {
+					return
+				}
+				productLinks = append(productLinks, productLink)
+			})
+		})
+
+		pageNumber := strconv.Itoa((activePage + 1))
+		c.Visit(crawlUrl + "&page=" + pageNumber)
+	})
+
+	for _, element := range categoriesUrls {
+		crawlUrl = element
+		c.Visit(element)
+	}
+}
+
+func fetchItemInfo() {
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", RandomString())
+	})
+
+	c.OnHTML("span.priceBlockDealPriceString", func(e *colly.HTMLElement) {
+		fmt.Println("PRICE-------------- " + e.Text)
+	})
+
+	c.OnHTML("span#productTitle", func(e *colly.HTMLElement) {
+		fmt.Println("PRODUCT-------------- " + e.Text)
+	})
+
+	for _, element := range productLinks {
+		fmt.Println("--------------------")
+		c.Visit(element)
+		fmt.Println("--------------------")
+	}
 }
